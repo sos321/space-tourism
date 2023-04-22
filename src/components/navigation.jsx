275bb4f90d22fd8354877a3934eb525b4f63a982
Filip Kocian <filkocian@gmail.com>
2023-04-22 11:05:33 +0200

    create navbar

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,21 +1,34 @@
 import { Fragment } from "react";
 import { Outlet, NavLink } from "react-router-dom";
 
+import logo from "./../assets/shared/logo.svg";
+
 function Navigation() {
   return (
     <Fragment>
-      <div>
-        <p>Navigation</p>
-        {["Home", "Destination", "Crew", "Technology"].map((nav) => (
-          <NavLink
-            to={`/${nav === "Home" ? "" : nav.toLowerCase()}`}
-            className={({ isActive }) => (isActive ? "underline" : "")}
-          >
-            {nav}
-          </NavLink>
-        ))}
+      <div className="flex items-center justify-between h-16 mt-16">
+        <img src={logo} alt="Logo" className="ml-12" />
+        <div className="h-[1px] bg-white w-3/12 opacity-25 z-20 absolute left-72"></div>
+        <div className="flex items-center justify-center w-7/12 h-24 gap-20 bg-white backdrop-blur-2xl bg-opacity-5">
+          {["Home", "Destination", "Crew", "Technology"].map((nav, i) => (
+            <NavLink
+              to={`/${nav === "Home" ? "" : nav.toLowerCase()}`}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-white border-solid border-b-2"
+                    : "border-transparent border-solid border-b-2 hover:border-white hover:border-solid hover:border-b-2 hover:border-opacity-50"
+                } text-xl uppercase tracking-widest flex gap-2 h-full items-center transition-all`
+              }
+              key={i}
+            >
+              <span className="font-bold">{`0${i}`}</span>
+              {nav}
+            </NavLink>
+          ))}
+        </div>
       </div>
-      <div>
+      <div className="m-20 bg-red-700">
         <Outlet />
       </div>
     </Fragment>
