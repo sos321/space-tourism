c0dbc02de90305d3312b32ccd0ac9545a3fd6c6f
Filip Kocian <filkocian@gmail.com>
2023-04-27 11:38:39 +0200

    navigation animations

---
+++ src/components/animated-link.jsx
@@ -0,0 +1,66 @@
+import { NavLink } from "react-router-dom";
+import { motion, useCycle } from "framer-motion";
+import { Fragment } from "react";
+
+export default function AnimatedLink(props) {
+  const [origin, cycleOrigin] = useCycle(0, 1);
+
+  const hoverVariants = {
+    idle: {
+      scaleX: 0,
+    },
+    hover: {
+      scaleX: 1,
+    },
+  };
+
+  return (
+    <motion.div
+      className="h-full closest"
+      whileHover="hover"
+      onHoverStart={() => {
+        cycleOrigin(props.dir);
+      }}
+      onHoverEnd={() => {
+        cycleOrigin(Math.abs(props.dir - 1));
+      }}
+      animate="idle"
+      initial="idle"
+    >
+      <NavLink
+        to={`/${props.nav === "Home" ? "" : props.nav.toLowerCase()}`}
+        className="relative flex items-center h-full gap-2 text-xl tracking-widest uppercase transition-all"
+      >
+        {({ isActive }) => {
+          return (
+            <Fragment>
+              <span className="font-bold">{`0${props.i}`}</span>
+              {props.nav}
+              {isActive ? (
+                <motion.span
+                  className="h-[2px] -mt-[2px] bg-white w-full absolute bottom-0"
+                  layout
+                  layoutId="active"
+                ></motion.span>
+              ) : (
+                <motion.span
+                  variants={hoverVariants}
+                  style={{
+                    position: "absolute",
+                    height: "2px",
+                    width: "100%",
+                    marginTop: "-2px",
+                    bottom: 0,
+                    backgroundColor: "#fff",
+                    opacity: 0.4,
+                    originX: origin,
+                  }}
+                ></motion.span>
+              )}
+            </Fragment>
+          );
+        }}
+      </NavLink>
+    </motion.div>
+  );
+}
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,34 +1,41 @@
-import { Fragment } from "react";
-import { Outlet, NavLink } from "react-router-dom";
+import { Fragment, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { LayoutGroup } from "framer-motion";
 
 import NavLine from "./UI/nav-line";
+import AnimatedLink from "./animated-link";
 
 import logo from "./../assets/shared/logo.svg";
 
+let xDir = 0;
+
 function Navigation() {
+  const [dir, setDir] = useState(0);
+
+  function updateDirection(e) {
+    const xDirNew = e.clientX;
+
+    if (xDirNew > xDir) setDir(0);
+    else if (xDirNew <= xDir) setDir(1);
+
+    xDir = xDirNew;
+  }
+
   return (
     <Fragment>
       <div className="flex items-center justify-between h-16 mt-16">
         <img src={logo} alt="Logo" className="ml-12" />
         <NavLine />
-        <div className="flex items-center justify-center w-7/12 h-24 gap-20 bg-white backdrop-blur-2xl bg-opacity-5">
-          {["Home", "Destination", "Crew", "Technology"].map((nav, i) => (
-            <NavLink
-              to={`/${nav === "Home" ? "" : nav.toLowerCase()}`}
-              className={({ isActive }) =>
-                `${
-                  isActive
-                    ? "border-white border-solid border-b-2"
-                    : "border-transparent border-solid border-b-2 hover:border-white hover:border-solid hover:border-b-2 hover:border-opacity-50"
-                } text-xl uppercase tracking-widest flex gap-2 h-full items-center transition-all`
-              }
-              key={i}
-            >
-              <span className="font-bold">{`0${i}`}</span>
-              {nav}
-            </NavLink>
-          ))}
-        </div>
+        <LayoutGroup>
+          <div
+            className="flex items-center justify-center w-7/12 h-24 gap-20 bg-white backdrop-blur-2xl bg-opacity-5"
+            onMouseMoveCapture={updateDirection}
+          >
+            {["Home", "Destination", "Crew", "Technology"].map((nav, i) => (
+              <AnimatedLink nav={nav} i={i} dir={dir} key={i} />
+            ))}
+          </div>
+        </LayoutGroup>
       </div>
       <div className="mt-48 mx-36">
         <Outlet />
